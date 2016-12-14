e7e3ebb6d7a14155f7bb17c0819805b786b73a94
Andrew Tridgell <andrew@tridgell.net>
2016-12-14 14:13:33 +1100

    AP_InertialSensor: added support for ICM-20789 to Invensense driver

--- libraries/AP_InertialSensor/AP_InertialSensor_Invensense.cpp
+++ libraries/AP_InertialSensor/AP_InertialSensor_Invensense.cpp
@@ -220,6 +220,7 @@ extern const AP_HAL::HAL& hal;
 #define MPU_WHOAMI_20608		0xaf
 #define MPU_WHOAMI_MPU9250      0x71
 #define MPU_WHOAMI_MPU9255      0x73
+#define MPU_WHOAMI_ICM20789     0x02
 
 #define BIT_READ_FLAG                           0x80
 #define BIT_I2C_SLVX_EN                         0x80
@@ -398,6 +399,10 @@ void AP_InertialSensor_Invensense::start()
     default:
         gdev = DEVTYPE_GYR_MPU6000;
         adev = DEVTYPE_ACC_MPU6000;
+        break;
+    case Invensense_ICM20789:
+        gdev = DEVTYPE_INS_ICM20789;
+        adev = DEVTYPE_INS_ICM20789;
         break;
     }
     _gyro_instance = _imu.register_gyro(1000, _dev->get_bus_id_devtype(gdev));
@@ -710,7 +715,7 @@ void AP_InertialSensor_Invensense::_read_fifo()
     }
 
     if (need_reset) {
-        //debug("fifo reset n_samples %u", bytes_read/MPU_SAMPLE_SIZE);
+        debug("fifo reset n_samples %u", bytes_read/MPU_SAMPLE_SIZE);
         _fifo_reset();
     }
     
@@ -787,7 +792,7 @@ void AP_InertialSensor_Invensense::_set_filter_register(void)
         config |= BITS_DLPF_CFG_188HZ;
     }
 
-    config |= MPUREG_CONFIG_FIFO_MODE_STOP;
+    //config |= MPUREG_CONFIG_FIFO_MODE_STOP;
     _register_write(MPUREG_CONFIG, config, true);
 
 	if (_mpu_type != Invensense_MPU6000) {
@@ -795,7 +800,7 @@ void AP_InertialSensor_Invensense::_set_filter_register(void)
             // setup for 4kHz accels
             _register_write(ICMREG_ACCEL_CONFIG2, ICM_ACC_FCHOICE_B, true);
         } else {
-            _register_write(ICMREG_ACCEL_CONFIG2, ICM_ACC_DLPF_CFG_218HZ, true);
+            _register_write(ICMREG_ACCEL_CONFIG2, ICM_ACC_DLPF_CFG_218HZ | 0x40, true);
         }
     }
 }
@@ -816,6 +821,9 @@ bool AP_InertialSensor_Invensense::_check_whoami(void)
         return true;
     case MPU_WHOAMI_20608:
         _mpu_type = Invensense_ICM20608;
+        return true;
+    case MPU_WHOAMI_ICM20789:
+        _mpu_type = Invensense_ICM20789;
         return true;
     }
     // not a value WHOAMI result
