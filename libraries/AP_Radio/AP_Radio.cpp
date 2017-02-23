062dbc9d9968825bd3c1bfd5906bc929447c45c8
Andrew Tridgell <andrew@tridgell.net>
2017-02-23 13:17:40 +1100

    AP_Radio: protect against calls before init()

--- libraries/AP_Radio/AP_Radio.cpp
+++ libraries/AP_Radio/AP_Radio.cpp
@@ -8,21 +8,33 @@ extern const AP_HAL::HAL& hal;
 bool AP_Radio::init(void)
 {
     driver = new AP_Radio_cypress(*this);
+    if (!driver) {
+        return false;
+    }
     return driver->init();
 }
 
 bool AP_Radio::reset(void)
 {
+    if (!driver) {
+        return false;
+    }
     return driver->reset();
 }
 
 bool AP_Radio::send(const uint8_t *pkt, uint16_t len)
 {
+    if (!driver) {
+        return false;
+    }
     return driver->send(pkt, len);
 }
 
 void AP_Radio::start_recv_bind(void)
 {
+    if (!driver) {
+        return;
+    }
     return driver->start_recv_bind();
 }
 
@@ -33,10 +45,24 @@ const AP_Radio::stats &AP_Radio::get_stats(void)
 
 uint8_t AP_Radio::num_channels(void)
 {
+    if (!driver) {
+        return 0;
+    }
     return driver->num_channels();
 }
 
 uint16_t AP_Radio::read(uint8_t chan)
 {
+    if (!driver) {
+        return 0;
+    }
     return driver->read(chan);
+}
+
+uint32_t AP_Radio::last_recv_us(void)
+{
+    if (!driver) {
+        return 0;
+    }
+    return driver->last_recv_us();
 }
