f3808740fb067735a69fd1e84a39a1c8dffa9306
Andrew Tridgell <andrew@tridgell.net>
2017-06-03 18:21:11 +1000

    Copter: added settable toy modes

--- ArduCopter/toy_mode.h
+++ ArduCopter/toy_mode.h
@@ -10,15 +10,19 @@ class ToyMode
 public:
     friend class Copter;
 
+    ToyMode();
     void update(void);
 
     static const struct AP_Param::GroupInfo var_info[];
     
 private:
+    bool first_update;
     AP_Int8 enable;
+    AP_Int8 primary_mode1;
+    AP_Int8 primary_mode2;
     
     int32_t ch6_counter;
     uint32_t power_counter;
     uint32_t throttle_low_counter;
-    bool last_gps_enable;
+    bool last_secondary_mode;
 };
