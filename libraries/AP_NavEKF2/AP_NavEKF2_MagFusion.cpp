fb83d17c6dc95729e23f9bb58a5dfe5bf9a9f1c2
Andrew Tridgell <andrew@tridgell.net>
2017-06-29 10:19:50 +1000

    AP_NavEKF2: change alt threshold for mag to 3.0 meters

--- libraries/AP_NavEKF2/AP_NavEKF2_MagFusion.cpp
+++ libraries/AP_NavEKF2/AP_NavEKF2_MagFusion.cpp
@@ -58,7 +58,7 @@ void NavEKF2_core::controlMagYawReset()
     if (flightResetAllowed && !assume_zero_sideslip()) {
         // check that we have reached a height where ground magnetic interference effects are insignificant
         // and can perform a final reset of the yaw and field states
-        finalResetRequest = (stateStruct.position.z  - posDownAtTakeoff) < -5.0f;
+        finalResetRequest = (stateStruct.position.z  - posDownAtTakeoff) < -3.0f;
 
         // check for increasing height
         bool hgtIncreasing = (posDownAtLastMagReset-stateStruct.position.z) > 0.5f;
