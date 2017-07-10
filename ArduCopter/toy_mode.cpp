cc0cdb2b104583694fb03515f6c10ab6a01d2c24
Andrew Tridgell <andrew@tridgell.net>
2017-07-10 14:47:20 +1000

    Copter: start slow down on descent earlier
    use a linear ramp so the change isn't so sudden

--- ArduCopter/toy_mode.cpp
+++ ArduCopter/toy_mode.cpp
@@ -11,6 +11,7 @@
 #define TOY_RIGHT_PRESS_COUNT 1
 #define TOY_ACTION_DELAY_MS 200
 #define TOY_DESCENT_SLOW_HEIGHT 5
+#define TOY_DESCENT_SLOW_RAMP 3
 #define TOY_DESCENT_SLOW_MIN 300
 
 const AP_Param::GroupInfo ToyMode::var_info[] = {
@@ -566,12 +567,18 @@ void ToyMode::throttle_adjust(float &throttle_control)
         float p = (now - throttle_arm_ms) / float(soft_start_ms);
         throttle_control = MIN(throttle_control, throttle_start + p * (1000 - throttle_start));
     }
-    
+
     // limit descent rate close to the ground
-    if (throttle_control < TOY_DESCENT_SLOW_MIN && copter.motors->armed() && !copter.ap.land_complete &&
-        (copter.inertial_nav.get_altitude() * 0.01 - copter.arming_altitude_m) < TOY_DESCENT_SLOW_HEIGHT) {
-        // limit descent rate close to the ground
-        throttle_control = TOY_DESCENT_SLOW_MIN;
+    float height = copter.inertial_nav.get_altitude() * 0.01 - copter.arming_altitude_m;
+    if (throttle_control < 500 &&
+        height < TOY_DESCENT_SLOW_HEIGHT + TOY_DESCENT_SLOW_RAMP &&
+        copter.motors->armed() && !copter.ap.land_complete) {
+        float limit = linear_interpolate(TOY_DESCENT_SLOW_MIN, 0, height,
+                                         TOY_DESCENT_SLOW_HEIGHT, TOY_DESCENT_SLOW_HEIGHT+TOY_DESCENT_SLOW_RAMP);
+        if (throttle_control < limit) {
+            // limit descent rate close to the ground
+            throttle_control = limit;
+        }
     }
 }
 
