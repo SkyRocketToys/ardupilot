a3b925ad120238754023d1bbd2aaebab3a1dc1d4
Andrew Tridgell <andrew@tridgell.net>
2017-07-14 18:26:58 +1000

    AP_NavEKF2: back out to copter beta 3.5 EKF alt handling

--- libraries/AP_NavEKF2/AP_NavEKF2_Outputs.cpp
+++ libraries/AP_NavEKF2/AP_NavEKF2_Outputs.cpp
@@ -279,27 +279,18 @@ bool NavEKF2_core::getPosNE(Vector2f &posNE) const
     return false;
 }
 
-// Write the last calculated D position of the body frame origin relative to the EKF origin (m).
+// Write the last calculated D position of the body frame origin relative to the reference point (m).
 // Return true if the estimate is valid
 bool NavEKF2_core::getPosD(float &posD) const
 {
     // The EKF always has a height estimate regardless of mode of operation
-    // Correct for the IMU offset (EKF calculations are at the IMU)
-    // Correct for
-    if (frontend->_originHgtMode & (1<<2)) {
-        // Any sensor height drift corrections relative to the WGS-84 reference are applied to the origin.
-        posD = outputDataNew.position.z + posOffsetNED.z;
-    } else {
-        // The origin height is static and corrections are applied to the local vertical position
-        // so that height returned by getLLH() = height returned by getOriginLLH - posD
-        posD = outputDataNew.position.z + posOffsetNED.z + 0.01f * (float)EKF_origin.alt - (float)ekfGpsRefHgt;
-    }
+    // correct for the IMU offset (EKF calculations are at the IMU)
+    posD = outputDataNew.position.z + posOffsetNED.z;
 
     // Return the current height solution status
     return filterStatus.flags.vert_pos;
 
 }
-
 // return the estimated height of body frame origin above ground level
 bool NavEKF2_core::getHAGL(float &HAGL) const
 {
@@ -317,7 +308,7 @@ bool NavEKF2_core::getLLH(struct Location &loc) const
 {
     if(validOrigin) {
         // Altitude returned is an absolute altitude relative to the WGS-84 spherioid
-        loc.alt =  100 * (int32_t)(ekfGpsRefHgt - (double)outputDataNew.position.z);
+        loc.alt = EKF_origin.alt - outputDataNew.position.z*100;
         loc.flags.relative_alt = 0;
         loc.flags.terrain_alt = 0;
 
@@ -379,10 +370,6 @@ bool NavEKF2_core::getOriginLLH(struct Location &loc) const
 {
     if (validOrigin) {
         loc = EKF_origin;
-        // report internally corrected reference height if enabled
-        if (frontend->_originHgtMode & (1<<2)) {
-            loc.alt = (int32_t)(100.0f * (float)ekfGpsRefHgt);
-        }
     }
     return validOrigin;
 }
