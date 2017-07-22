66f595733d20593507149efe583bcaed7b164d0a
Andrew Tridgell <andrew@tridgell.net>
2017-07-22 11:15:00 +1000

    Copter: update firmware version

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -2,7 +2,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "APM:Copter V3.6-dev"
+#define THISFIRMWARE "APM:Copter V3.6-SRT1"
 #define FIRMWARE_VERSION 3,6,0,FIRMWARE_VERSION_TYPE_DEV
 
 #ifndef GIT_VERSION
