03e8ad4194cd542e234a66efc90d1b5a4a901278
Andrew Tridgell <andrew@tridgell.net>
2017-07-28 14:16:54 +1000

    Copter: delat thr cancel of land on obstruction
    when we trigger LAND due to obstruction detection, don't allow
    throttle cancel for at least 2 seconds, to give the vehicle a chance
    to start descending.

--- ArduCopter/control_land.cpp
+++ ArduCopter/control_land.cpp
@@ -95,7 +95,12 @@ void Copter::land_nogps_run()
 
     // process pilot inputs
     if (!failsafe.radio) {
-        if ((g.throttle_behavior & THR_BEHAVE_HIGH_THROTTLE_CANCELS_LAND) != 0 && rc_throttle_control_in_filter.get() > LAND_CANCEL_TRIGGER_THR){
+        // check for land cancel by pilot, but not if we've triggered
+        // a crash in last 2 seconds, to cope with LAND from
+        // obstruction detection
+        if ((g.throttle_behavior & THR_BEHAVE_HIGH_THROTTLE_CANCELS_LAND) != 0 &&
+            rc_throttle_control_in_filter.get() > LAND_CANCEL_TRIGGER_THR &&
+            millis() - crash.last_trigger_ms > 2000) {
             Log_Write_Event(DATA_LAND_CANCELLED_BY_PILOT);
             // exit land if throttle is high
             set_mode(ALT_HOLD, MODE_REASON_THROTTLE_LAND_ESCAPE);
@@ -220,7 +225,12 @@ void Copter::land_run_horizontal_control()
     
     // process pilot inputs
     if (!failsafe.radio) {
-        if ((g.throttle_behavior & THR_BEHAVE_HIGH_THROTTLE_CANCELS_LAND) != 0 && rc_throttle_control_in_filter.get() > LAND_CANCEL_TRIGGER_THR){
+        // check for land cancel by pilot, but not if we've triggered
+        // a crash in last 2 seconds, to cope with LAND from
+        // obstruction detection
+        if ((g.throttle_behavior & THR_BEHAVE_HIGH_THROTTLE_CANCELS_LAND) != 0 &&
+            rc_throttle_control_in_filter.get() > LAND_CANCEL_TRIGGER_THR &&
+            millis() - crash.last_trigger_ms > 2000) {
             Log_Write_Event(DATA_LAND_CANCELLED_BY_PILOT);
             // exit land if throttle is high
             if (!set_mode(LOITER, MODE_REASON_THROTTLE_LAND_ESCAPE)) {
