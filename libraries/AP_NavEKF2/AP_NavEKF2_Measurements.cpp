11f30213c73e6fb37260474d7993ead2f4ef1688
Andrew Tridgell <andrew@tridgell.net>
2017-08-04 06:20:56 +1000

    AP_NavEKF2: disable GPS Z fusion when indoors

--- libraries/AP_NavEKF2/AP_NavEKF2_Measurements.cpp
+++ libraries/AP_NavEKF2/AP_NavEKF2_Measurements.cpp
@@ -472,7 +472,9 @@ void NavEKF2_core::readGpsData()
             }
 
             // Check if GPS can output vertical velocity and set GPS fusion mode accordingly
-            if (_ahrs->get_gps().have_vertical_velocity() && frontend->_fusionModeGPS == 0) {
+            if (_ahrs->get_gps().have_vertical_velocity() &&
+                frontend->_fusionModeGPS == 0 &&
+                !_ahrs->get_indoor_mode()) {
                 useGpsVertVel = true;
             } else {
                 useGpsVertVel = false;
