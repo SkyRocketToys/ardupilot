c4d016d409d7846cbc4ca2ac75fd9171869fd2d9
Andrew Tridgell <andrew@tridgell.net>
2017-08-21 18:49:50 +1000

    AP_Compass: use a min attitude change, not field change
    field can change due to learned offsets

--- libraries/AP_Compass/Compass_learn.h
+++ libraries/AP_Compass/Compass_learn.h
@@ -43,8 +43,8 @@ private:
     
     struct sample new_sample;
     bool sample_available;
-    Vector3f last_field;
-    static const uint32_t min_field_change = 60;
+    Vector3f last_attitude;
+    static const uint32_t min_attitude_change = 7;
 
     Vector3f best_offsets;
     float best_error;
