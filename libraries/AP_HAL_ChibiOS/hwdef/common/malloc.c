aa2a4f92441ba37e36e7b8c929392533bd9022cf
Andrew Tridgell <andrew@tridgell.net>
2017-09-27 15:34:17 +1000

    HAL_ChibiOS: wrap more libc allocation functions
    needed for asprintf()

--- libraries/AP_HAL_ChibiOS/hwdef/common/malloc.c
+++ libraries/AP_HAL_ChibiOS/hwdef/common/malloc.c
@@ -2,12 +2,18 @@
   wrappers for allocation functions
 
   Relies on linker wrap options
+
+  Note that not all functions that have been wrapped are implemented
+  here. The others are wrapped to ensure the function is not used
+  without an implementation. If we need them then we can implement as
+  needed.
  */
 
 #include <stdio.h>
 #include <string.h>
 #include <hal.h>
 #include <chheap.h>
+#include <stdarg.h>
 
 #define MIN_ALIGNMENT 8
 
@@ -28,4 +34,33 @@ void *__wrap_calloc(size_t nmemb, size_t size)
 void __wrap_free(void *ptr)
 {
     return chHeapFree(ptr);
+}
+
+int __wrap_vsnprintf(char *str, size_t size, const char *fmt, va_list ap)
+{
+    return __real_vsnprintf(str, size, fmt, ap);
+}
+
+int __wrap_vasprintf(char **strp, const char *fmt, va_list ap)
+{
+    int len = __wrap_vsnprintf(NULL, 0, fmt, ap);
+    if (len <= 0) {
+        return -1;
+    }
+    char *buf = __wrap_calloc(len+1, 1);
+    if (!buf) {
+        return -1;
+    }
+    __wrap_vsnprintf(buf, len, fmt, ap);
+    (*strp) = buf;
+    return len;
+}
+
+int __wrap_asprintf(char **strp, const char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    int ret = __wrap_vasprintf(strp, fmt, ap);
+    va_end(ap);
+    return ret;
 }
