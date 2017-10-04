b1749e93bd9b94fa724a93b838586d805faebed0
Czar <czar.balangue@yahoo.com>
2017-10-04 10:03:01 +0100

    Copter: Prevents geofence being enabled if copter fails to switch to loiter from alt_hold

--- ArduCopter/toy_mode.cpp
+++ ArduCopter/toy_mode.cpp
@@ -192,7 +192,13 @@ void ToyMode::update()
     if (!copter.motors->armed()) {
         load_test.running = false;
     }
-
+	
+	//prevent geofence being enabled when in gps denied environment 
+	if (copter.control_mode == ALT_HOLD) 
+	{ 
+		copter.fence.enable(false); 
+	}
+	
     // keep filtered battery voltage for thrust limiting
     filtered_voltage = 0.99 * filtered_voltage + 0.01 * copter.battery.voltage();
     
