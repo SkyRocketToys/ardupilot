2a72bba20cf8ac29ba9679ebab273c8212d63008
Andrew Tridgell <andrew@tridgell.net>
2017-10-14 09:13:30 +1100

    AP_AHRS: prevent occasional SITL crash on startup

--- libraries/AP_AHRS/AP_AHRS_NavEKF.cpp
+++ libraries/AP_AHRS/AP_AHRS_NavEKF.cpp
@@ -1429,6 +1429,9 @@ bool AP_AHRS_NavEKF::get_origin(Location &ret) const
 
 #if CONFIG_HAL_BOARD == HAL_BOARD_SITL
     case EKF_TYPE_SITL:
+        if (!_sitl) {
+            return false;
+        }
         const struct SITL::sitl_fdm &fdm = _sitl->state;
         ret = fdm.home;
         return true;
