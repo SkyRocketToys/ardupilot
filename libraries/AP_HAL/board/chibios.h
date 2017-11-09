96c589fdd4e64c8670cba059bdbdc70f37cef66d
Andrew Tridgell <andrew@tridgell.net>
2017-11-09 18:52:06 +1100

    AP_HAL: fixed chibios default LEDs

--- libraries/AP_HAL/board/chibios.h
+++ libraries/AP_HAL/board/chibios.h
@@ -5,8 +5,8 @@
 #define HAL_MINIMIZE_FEATURES       1
 
 #define HAL_GPIO_A_LED_PIN        0
-#define HAL_GPIO_B_LED_PIN        1
-#define HAL_GPIO_C_LED_PIN        2
+#define HAL_GPIO_B_LED_PIN        0
+#define HAL_GPIO_C_LED_PIN        0
 #define HAL_GPIO_LED_ON           LOW
 #define HAL_GPIO_LED_OFF          HIGH
 
