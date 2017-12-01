dd4d9dc55159705b7b123d1b6af16e6428e0d09e
Andrew Tridgell <andrew@tridgell.net>
2017-12-01 13:40:05 +1100

    Tools: generate the DMA defines for boards in dma_resolver.py

--- Tools/scripts/dma_resolver.py
+++ Tools/scripts/dma_resolver.py
@@ -1,5 +1,20 @@
+#!/usr/bin/env python
+
+import optparse, sys
+
+boards = ['F412', 'FMUv3']
+
+parser = optparse.OptionParser("dma_resolver.py")
+parser.add_option("-B", "--board", type='choice', default=None, choices=boards, help='board type')
+
+opts, args = parser.parse_args()
+
+if opts.board is None:
+        print("Please choose a board from: %s" % boards)
+        sys.exit(1)
 
 STM32F427_DMA_Map = {
+        # format is [DMA_TABLE, StreamNum]
 	"SPI1_RX"	:	[[2,0],[2,2]],
 	"SPI1_TX"	:	[[2,3],[2,5]],
 	"SPI2_RX"	:	[[1,3]],
@@ -18,13 +33,13 @@ STM32F427_DMA_Map = {
 	"UART4_TX"	:	[[1,4]],
 	"UART5_RX"	:	[[1,0]],
 	"UART5_TX"	:	[[1,7]],
-	"USART6_RX" :	[[2,1],[2,2]],
-	"USART6_TX" :	[[2,6],[2,7]],
+	"USART6_RX"	:	[[2,1],[2,2]],
+	"USART6_TX"	:	[[2,6],[2,7]],
 	"UART7_RX"	:	[[1,3]],
 	"UART7_TX"	:	[[1,1]],
 	"UART8_RX"	:	[[1,6]],
 	"UART8_TX"	:	[[1,0]],
-	"ADC1"		:	[[1,0],[1,4]],
+	"ADC1"		:	[[2,0],[2,4]],
 	"ADC2"		:	[[1,2],[1,3]],
 	"ADC3"		:	[[1,0],[1,1]],
 	"I2C1_RX"	:	[[1,0],[1,5]],
@@ -32,10 +47,12 @@ STM32F427_DMA_Map = {
 	"I2C2_RX"	:	[[1,2],[1,3]],
 	"I2C2_TX"	:	[[1,7]],
 	"I2C3_RX"	:	[[1,2]],
-	"I2C3_TX"	:	[[1,4]]
+	"I2C3_TX"	:	[[1,4]],
+        "SDIO"		:	[[2,3],[2,6]],
 }
 
 STM32F412_DMA_Map = {
+        # format is [DMA_TABLE, StreamNum]
 	"SPI1_RX"	:	[[2,0],[2,2]],
 	"SPI1_TX"	:	[[2,2],[2,3],[2,5]],
 	"SPI2_RX"	:	[[1,3]],
@@ -52,8 +69,8 @@ STM32F412_DMA_Map = {
 	"USART2_TX"	:	[[1,6]],
 	"USART3_RX"	:	[[1,1]],
 	"USART3_TX"	:	[[1,3],[1,4]],
-	"USART6_RX" :	[[2,1],[2,2]],
-	"USART6_TX" :	[[2,6],[2,7]],
+	"USART6_RX"	:	[[2,1],[2,2]],
+	"USART6_TX"	:	[[2,6],[2,7]],
 	"ADC1"		:	[[2,0],[2,4]],
 	"I2C1_RX"	:	[[1,0],[1,5]],
 	"I2C1_TX"	:	[[1,1],[1,6],[1,7]],
@@ -63,8 +80,24 @@ STM32F412_DMA_Map = {
 	"I2C3_TX"	:	[[1,4],[1,5]],
 }
 
-PERIPHONDMA_LIST = ["ADC1","SPI1_RX","SPI1_TX","SPI2_RX","SPI2_TX","SPI5_RX","SPI5_TX",\
-					"I2C1_RX","I2C1_TX","USART3_TX","USART3_RX","USART6_TX","USART6_RX"]
+if opts.board == 'FMUv3':
+        dma_map = STM32F427_DMA_Map
+        PERIPHONDMA_LIST = ["SDIO"]
+        PERIPHONDMA_LIST += ["ADC1"]
+        PERIPHONDMA_LIST += ["SPI1_RX","SPI1_TX","SPI2_RX","SPI2_TX","SPI4_RX","SPI4_TX"]
+        PERIPHONDMA_LIST += ["I2C1_RX","I2C1_TX","I2C2_RX","I2C2_TX"]
+        PERIPHONDMA_LIST += ["USART2_TX","USART2_RX"]
+        PERIPHONDMA_LIST += ["USART6_TX","USART6_RX"]
+        PERIPHONDMA_LIST += ["USART3_TX","USART3_RX"]
+elif opts.board == 'F412':
+        dma_map = STM32F412_DMA_Map
+        PERIPHONDMA_LIST = ["ADC1"]
+        PERIPHONDMA_LIST += ["SPI1_RX","SPI1_TX","SPI2_RX","SPI2_TX","SPI5_RX","SPI5_TX"]
+        PERIPHONDMA_LIST += ["I2C1_RX","I2C1_TX","I2C2_RX", "I2C2_TX"]
+        PERIPHONDMA_LIST += ["USART2_TX","USART2_RX"]
+        PERIPHONDMA_LIST += ["USART3_TX","USART3_RX"]
+        PERIPHONDMA_LIST += ["USART6_TX","USART6_RX"]
+
 
 def check_possibility(periph, dma_stream, curr_dict, dma_map, check_list):
 	for other_periph in curr_dict:
@@ -82,7 +115,7 @@ def check_possibility(periph, dma_stream, curr_dict, dma_map, check_list):
 				if check_str in check_list:
 					return False
 				check_list.append(check_str)
-				print "Trying to Resolve Conflict: ", check_str
+				print("Trying to Resolve Conflict: ", check_str)
 				#check if we can resolve by swapping with other periphs
 				for stream in dma_map[other_periph]:
 					if stream != curr_dict[other_periph] and \
@@ -93,7 +126,6 @@ def check_possibility(periph, dma_stream, curr_dict, dma_map, check_list):
 	return True
 unassigned = []
 curr_dict = {}
-dma_map = STM32F412_DMA_Map
 for periph in PERIPHONDMA_LIST:
 	assigned = False
 	ignore_list = []
@@ -106,9 +138,27 @@ for periph in PERIPHONDMA_LIST:
 	if assigned == False:
 		unassigned.append(periph)
 
-print "\n\nMOST VIABLE DMA CONFIG:\n"
-print "{:<15} {:<15}".format('Peripherals','Stream')
-for key in sorted(curr_dict.iterkeys()):
-	print "{:<15} {:<15}".format(key,curr_dict[key])
+def chibios_dma_define_name(key):
+        '''return define name needed for board.h for ChibiOS'''
+        if key.startswith('ADC'):
+                return 'STM32_ADC_%s_DMA_STREAM' % key
+        elif key.startswith('SPI'):
+                return 'STM32_SPI_%s_DMA_STREAM' % key
+        elif key.startswith('I2C'):
+                return 'STM32_I2C_%s_DMA_STREAM' % key
+        elif key.startswith('USART'):
+                return 'STM32_UART_%s_DMA_STREAM' % key
+        elif key.startswith('SDIO'):
+                return 'STM32_SDC_%s_DMA_STREAM' % key
+        else:
+                print("Error: Unknown key type %s" % key)
+                sys.exit(1)
+                
 
-print "\n\nFollowing Peripherals can't be resolved:", unassigned
+print("\n\nMOST VIABLE DMA CONFIG:\n")
+print("// auto-generated DMA mapping from dma_resolver.py")
+for key in sorted(curr_dict.iterkeys()):
+        print("#define %-30s STM32_DMA_STREAM_ID(%u, %u)" % (chibios_dma_define_name(key), curr_dict[key][0], curr_dict[key][1]))
+
+if unassigned:
+        print("\nWARNING: Following Peripherals can't be resolved: %s" % unassigned)
