1c592231bff31da61c01c0a7dcaa92e46f99f0c8
Peter Barker <pbarker@barker.dropbear.id.au>
2018-02-22 17:45:54 +1100

    AP_Compass: remove pointless initialisations

--- libraries/AP_Compass/AP_Compass.cpp
+++ libraries/AP_Compass/AP_Compass.cpp
@@ -442,15 +442,7 @@ const AP_Param::GroupInfo Compass::var_info[] = {
 // their values.
 //
 Compass::Compass(void) :
-    _compass_cal_autoreboot(false),
-    _cal_complete_requires_reboot(false),
-    _cal_has_run(false),
-    _backend_count(0),
-    _compass_count(0),
-    _board_orientation(ROTATION_NONE),
-    _null_init_done(false),
-    _thr_or_curr(0.0f),
-    _hil_mode(false)
+    _board_orientation(ROTATION_NONE)
 {
     AP_Param::setup_object_defaults(this, var_info);
     for (uint8_t i=0; i<COMPASS_MAX_BACKEND; i++) {
