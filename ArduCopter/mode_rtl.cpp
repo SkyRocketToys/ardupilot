db521be2362d6f05e1ba4232466a5c03a7412d57
Peter Barker <pbarker@barker.dropbear.id.au>
2018-04-04 15:43:53 +1000

    Copter: correct compilation when RTL disabled

--- ArduCopter/mode_rtl.cpp
+++ ArduCopter/mode_rtl.cpp
@@ -1,5 +1,7 @@
 #include "Copter.h"
 
+#if MODE_RTL_ENABLED == ENABLED
+
 /*
  * Init and run calls for RTL flight mode
  *
@@ -561,3 +563,5 @@ int32_t Copter::ModeRTL::wp_bearing() const
 {
     return wp_nav->get_wp_bearing_to_destination();
 }
+
+#endif
